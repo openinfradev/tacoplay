515bf2f72e1a2d900bee58ebf46fbf0613f2673c
yongiman <sjin.han@sk.com>
2019-03-21 09:45:46 +0000

    run ironic containers not install packages

--- roles/ironic/files/centos-user-image-update.sh
+++ roles/ironic/files/centos-user-image-update.sh
@@ -1,10 +1,10 @@
 ### Usage : ./centos-user-image-update.sh {node-name}
 
-MD5=`md5sum /var/lib/ironic/centos7-user/user-centos7.qcow2 | awk '{print $1}'`
+MD5=`md5sum /var/lib/tftpboot/ironic_images/user-centos7.qcow2 | awk '{print $1}'`
 
 ironic node-update $1 add \
-instance_info/image_source=file:///var/lib/ironic/centos7-user/user-centos7.qcow2  \
-instance_info/kernel=file:///var/lib/ironic/centos7-user/user-centos7.kernel  \
-instance_info/ramdisk=file:///var/lib/ironic/centos7-user/user-centos7.initramfs \
+instance_info/image_source=file:///var/lib/tftpboot/ironic_images/user-centos7.qcow2  \
+instance_info/kernel=file:///var/lib/tftpboot/ironic_images/user-centos7.kernel  \
+instance_info/ramdisk=file:///var/lib/tftpboot/ironic_images/user-centos7.initramfs \
 instance_info/image_checksum=$MD5 \
 instance_info/root_gb=10
--- roles/ironic/files/real-ubuntu-deploy.sh
+++ roles/ironic/files/real-ubuntu-deploy.sh
@@ -3,11 +3,11 @@
 ironic node-update $1 add \
 driver_info/ipmi_address=$2 \
 driver_info/ipmi_username=admin \
-driver_info/ipmi_password=helion123! \
-driver_info/deploy_kernel=file:///var/lib/ironic/ubuntu-dev-deploy/ubuntu-devuser-image.kernel \
-driver_info/deploy_ramdisk=file:///var/lib/ironic/ubuntu-dev-deploy/ubuntu-devuser-image.initramfs
+driver_info/ipmi_password=admin \
+driver_info/deploy_kernel=file:///var/lib/tftpboot/ironic_images/ubuntu-deploy.kernel \
+driver_info/deploy_ramdisk=file:///var/lib/tftpboot/ironic_images/ubuntu-deploy.initramfs
 
-ironic node-set-power-state $1 off
+#ironic node-set-power-state $1 off
 
 # for master01-k2 server
 #ironic port-create -a 90:e2:ba:b4:f9:38 -n 00000000-0000-0000-0000-000000000001
