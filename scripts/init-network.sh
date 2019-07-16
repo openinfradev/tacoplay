bbfc15fe7fac2efad37edabf04d4d0b30355d52d
DaeSeong Kim <powerds0111@gmail.com>
2019-07-16 03:23:06 +0000

    add forward rule to accept oudbound

--- scripts/init-network.sh
+++ scripts/init-network.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ip addr add 10.10.10.1/24 dev br-ex
 ip link set br-ex up
+iptables -A FORWARD -o br-ex -j ACCEPT
+iptables -A FORWARD -o bond0 -j ACCEPT
 iptables -t nat -A POSTROUTING -o bond0 -j MASQUERADE
