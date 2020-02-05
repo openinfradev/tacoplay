58951236bf00e52d6124a5cf1c80cb45d2f33cde
Robert Choi <taeilchoi1@gmail.com>
2020-02-05 18:59:30 +0900

    refactor: refactor site-prepare play

--- scripts/pull-images.sh
+++ scripts/pull-images.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 #set -x
 MANIFESTS=$1
-CICD_REPO=$2
+CICD_REGISTRY=$2
+orig_registry_parsed=false
+
 if [ "x$MANIFESTS" == "x" ]; then
   echo "Usage: ./pull-images.sh <ARMADA MANIFEST>"
   exit -1
 fi
 
-MANIFEST_REPO=$(grep "nova_compute:" $MANIFESTS | head -1 | awk '{print $2}' | awk -F "/" '/1/ {print $1}')
 for IMAGE in $(cat $MANIFESTS | yq '.data.values.images.tags | map(.) | join(" ")' | tr -d '"'); do
-	NEW_IMAGE=$(sed "s/$MANIFEST_REPO/$CICD_REPO/g" <<< $IMAGE)
+  if (!orig_registry_parsed)
+    ORIG_REGISTRY=$((awk '{print $2}' | awk -F "/" '/1/ {print $1}') <<< $IMAGE)
+    orig_registry_parsed=true
+  fi
+
+	NEW_IMAGE=$(sed "s/$ORIG_REGISTRY/$CICD_REGISTRY/g" <<< $IMAGE)
 	echo $NEW_IMAGE
   docker inspect $NEW_IMAGE > /dev/null 2>&1 || docker pull $NEW_IMAGE
 done
