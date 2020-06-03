687934eaaf1ff2f6b940a36de5660fe960a44403
[SKT] 김대성 <daeseong.kim@sk.com>
2020-06-03 11:00:09 +0900

    fix : [TACODEV-261] fix tagging script

--- scripts/tag-images.sh
+++ scripts/tag-images.sh
@@ -10,36 +10,20 @@ NEW_REGISTRY="tacorepo:5000"
 #}
 
 if [ $# == 0 ]; then
-  for IMAGE in $(docker images | grep ago | awk '{ print $1":"$2 }'); do
-    IMAGE_NS=`echo $IMAGE | cut -d'/' -f1`
-    if [ "$IMAGE_NS" == "calico" ] \
-      || [ "$IMAGE_NS" == "cilium" ] \
-      || [ "$IMAGE_NS" == "sktdev" ] \
-      || [ "$IMAGE_NS" == "cloudnativelabs" ] \
-      || [ "$IMAGE_NS" == "contiv" ] \
-      || [ "$IMAGE_NS" == "coredns" ] \
-      || [ "$IMAGE_NS" == "ferest" ] \
-      || [ "$IMAGE_NS" == "lachlanevenson" ] \
-      || [ "$IMAGE_NS" == "library" ] \
-      || [ "$IMAGE_NS" == "nfvpe" ] \
-      || [ "$IMAGE_NS" == "rancher" ] \
-      || [ "$IMAGE_NS" == "weaveworks" ] \
-      || [ "$IMAGE_NS" == "xueshanf" ]; then
-      echo "preserving image name as it is."
-      IMAGE_NAME=`echo $IMAGE`
-    else
-      depth=$(echo $IMAGE | sed 's/[^/]//g' | awk '{print length}')
-      if [ $depth -eq 2 ]; then
-         IMAGE_NAME=`echo $IMAGE | cut -d'/' -f2,3`
-      elif [ $depth -eq 1 ]; then
-         IMAGE_NAME=`echo $IMAGE | cut -d'/' -f2`
-      elif [ $depth -eq 0 ]; then
-         IMAGE_NAME=`echo $IMAGE`
-      fi
-    fi
-    echo "*** command:" docker tag $IMAGE "$NEW_REGISTRY/$IMAGE_NAME"
-    docker tag $IMAGE "$NEW_REGISTRY/$IMAGE_NAME"
-  done
+  # repository name with *.io, *:INTEGER, *.cicd.stg.taco will be replaced to NEW_REGISTRY.
+  docker images --format "{{.Repository}}:{{.Tag}} {{.Repository}}:{{.Tag}}" \
+	| grep "\/" \
+	| sed -e "s/[a-z0-9.-]*.io\///2" \
+	| sed -e "s/[a-z0-9.-]*:[0-9]*\///2" \
+	| sed -e "s/[a-z0-9.-]*.cicd.stg.taco\///2" \
+	| sed -e "s/ / $NEW_REGISTRY\//g" \
+	| xargs -n2 docker tag $1 $2
+
+  # repository name without slash means it is a docker official image.
+	docker images --format "{{.Repository}}:{{.Tag}} {{.Repository}}:{{.Tag}}" \
+	| grep -v "\/" \
+	| sed -e "s/ / $NEW_REGISTRY\/library\//g" \
+	| xargs -n2 docker tag $1 $2
 fi
 
 if [ $# == 1 ]; then
