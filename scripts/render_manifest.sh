2cc6c6b08e28c53749870bf41dc220dd3882c07f
Jaesang Lee <jaesang_lee@sk.com>
2021-03-03 08:53:33 +0000

    Add decapod_base_yaml version for rendering

--- scripts/render_manifest.sh
+++ scripts/render_manifest.sh
@@ -2,5 +2,5 @@
 app=$1
 
 cd ${decapod_site_yaml_dest}
-.github/workflows/render.sh $app
+.github/workflows/render.sh $app $decapod_base_yaml_version
 cp ${app}/output/${site_name}/${app}-manifest.yaml ${inventory_dir}/
